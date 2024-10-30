1f9b09ca1f6fcbe0d857ff3ca3d64f9603609405
José Dores <jdores@cloudflare.com>
2024-10-30 13:47:27 +0000

    Updates to worker script

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ async function handleRequest(request, env) {
 	  <head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Youtube videos allowed for viewing</title>
+		<title>Youtube videos allowed</title>
 		<style>
 		  body { font-family: Arial, sans-serif; margin: 40px; }
 		  h1 { color: #333; }
